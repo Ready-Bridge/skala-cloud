a4e40f70890bc7ca7cde2f2c529d14176cf71d15
한준교 <hanjungyo5533@gmail.com>
2026-02-06 10:33:15 +0900

    chore(week7/vue): Vue 학습용 프로젝트 세팅

---
+++ week7/skala-vue-main/src/lectures/02-text/C1TextInterpolation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="inputValue" placeholder="내용을 입력하세요" />
+    <button @click="showMessage">확인</button>
+
+    <div> Message(보간법): {{ message }} </div>
+    <div v-text="textMessage"> </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const inputValue = ref('')
+const message = ref('')
+const textMessage = ref('Message(디렉티브): ')
+
+function showMessage() {
+  message.value = inputValue.value;
+  textMessage.value = 'Message(디렉티브): ' + inputValue.value;
+}
+</script>
